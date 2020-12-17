f9999a6f183236bddb09f6bd3f70023097cad442
heguohui2018 <heguohui2017@outlook.com>
2020-12-17 12:16:31 +0800

    提交

---
+++ swift_100days/day1.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+//let关键字声明一个常量
+let name = "hello world"
+print(name)
+
+let age = 20
+print(age)
+
+//var 声明一个变量
+var number = 20
+print(number)
+
+var videoname = "one day"
+print(videoname)
+
+//更改变量的值
+videoname = "two day"
+
+//类型注释
+let city:String = "china"
+print(city)
+
+let maxage:Int = 30
+print(maxage)
+
+let minage:Float = 30
+print(minage)
+
+
+//z字符串差值
